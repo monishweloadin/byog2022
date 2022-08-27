0dc7efa251c724581adfaa783f8365a7fec5f2ea
monishweloadin <monish@weloadin.com>
2022-08-27 14:03:25 +0530

    player sprint

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -4,8 +4,10 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] private float _moveSpeed = 5;
+    [SerializeField] private float _sprintSpeedMultiplier = 2;
+
     private CharacterController _characterController;
-    private float _moveSpeed = 5;
     private float _turnSpeed = 360;
     private Vector3 _input;
 
@@ -16,6 +18,8 @@ public class PlayerController : MonoBehaviour
     {
         _characterController = GetComponent<CharacterController>();  
         _animator = GetComponent<Animator>();
+
+        transform.GetChild(0).transform.position = new Vector3(transform.GetChild(0).transform.position.x, -2.2f, transform.GetChild(0).transform.position.z);
     }
 
     private void Update()
@@ -36,15 +40,7 @@ public class PlayerController : MonoBehaviour
 
     private void Look()
     {
-        if (_input == Vector3.zero) 
-        {
-            //_animator.SetTrigger("Idle");
-            _animator.SetBool("IsWalking", false);
-            _animator.SetBool("IsIdle", true);
-            return; 
-        }
-
-        //_animator.SetTrigger("Walk");
+        if (_input == Vector3.zero) return; 
         
         Quaternion rot = Quaternion.LookRotation(_input.ToIso(), Vector3.up);
         transform.rotation = Quaternion.RotateTowards(transform.rotation, rot, _turnSpeed * Time.deltaTime);
@@ -52,8 +48,15 @@ public class PlayerController : MonoBehaviour
 
     private void Move()
     {
-        _animator.SetBool("IsWalking", true);
-        _animator.SetBool("IsIdle", false);
-        _characterController.Move(transform.forward * _input.normalized.magnitude * _moveSpeed * Time.fixedDeltaTime);
+        float inputMagnitude = _input.normalized.magnitude;
+
+        if (Input.GetKey(KeyCode.LeftShift))
+        {
+            inputMagnitude *= _sprintSpeedMultiplier;
+        }
+
+        _animator.SetFloat("MovementBlend", inputMagnitude);
+
+        _characterController.Move(transform.forward * inputMagnitude * _moveSpeed * Time.fixedDeltaTime);
     }
 }
