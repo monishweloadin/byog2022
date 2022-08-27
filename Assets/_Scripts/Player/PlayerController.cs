6a83c562da39521a4b3e0b67179ced2397d814c9
monishweloadin <monish@weloadin.com>
2022-08-27 15:55:34 +0530

    punching script added

--- Assets/_Scripts/Player/PlayerController.cs
+++ Assets/_Scripts/Player/PlayerController.cs
@@ -8,6 +8,10 @@ public class PlayerController : MonoBehaviour
     [SerializeField] private float _sprintSpeedMultiplier = 2;
     [SerializeField] private float _jumpHeight = 3;
 
+    [SerializeField] private bool _isGrounded = true;
+
+    private int _punchCombo = 0;
+
     private CharacterController _characterController;
     private float _turnSpeed = 360;
     private Vector3 _input;
@@ -15,6 +19,10 @@ public class PlayerController : MonoBehaviour
     private float _gravity = -9.81f;
 
     private Animator _animator;
+
+    private int Punch1 = Animator.StringToHash("PlayerRightJab");
+    private int Punch2 = Animator.StringToHash("PlayerLeftJab");
+
 
 
     private void Start()
@@ -29,11 +37,23 @@ public class PlayerController : MonoBehaviour
     {
         GetInput();
         Look();
+
+        if (Input.GetMouseButtonDown(0))
+        {
+            Punch();
+        }
+        else
+        {
+            Move();
+            CheckForGrounded();
+            CheckForJump();
+        }
+
     }
 
     private void FixedUpdate()
     {
-        Move();
+        
     }
 
     private void GetInput()
@@ -53,6 +73,9 @@ public class PlayerController : MonoBehaviour
     {
         float inputMagnitude = _input.normalized.magnitude;
 
+        if (_animator.GetCurrentAnimatorStateInfo(0).IsName("PlayerLeftJab") || _animator.GetCurrentAnimatorStateInfo(0).IsName("PlayerRightJab"))
+            inputMagnitude = 0;
+
         if (Input.GetKey(KeyCode.LeftShift))
         {
             inputMagnitude *= _sprintSpeedMultiplier;
@@ -60,26 +83,57 @@ public class PlayerController : MonoBehaviour
 
         _animator.SetFloat("MovementBlend", inputMagnitude);
 
-        _characterController.Move(transform.forward * inputMagnitude * _moveSpeed * Time.fixedDeltaTime);
+        _characterController.Move(transform.forward * inputMagnitude * _moveSpeed * Time.deltaTime);
 
-        CheckForJump();
+        
     }
 
+
+    public Vector3 playerVelocity;
     private void CheckForJump()
     {
-        Vector3 playerVelocity = Vector3.zero;
+        if (_isGrounded && playerVelocity.y < -1f)
+            playerVelocity.y = 0;
 
-        print(_characterController.isGrounded);
-
-        if(Input.GetKeyDown(KeyCode.Space) && _characterController.isGrounded)
+        if (Input.GetKeyDown(KeyCode.Space) && _isGrounded)
         {
-            print("jumpo");
-            playerVelocity.y += Mathf.Sqrt(_jumpHeight * -1 * _gravity);
+            playerVelocity.y += Mathf.Sqrt(_jumpHeight * -3f * _gravity);
         }
 
-        playerVelocity.y += _gravity * Time.fixedDeltaTime;
+        playerVelocity.y += _gravity * 10 * Time.deltaTime;
+        _characterController.Move(playerVelocity * Time.deltaTime);
+    }
 
-        _characterController.Move(playerVelocity * Time.fixedDeltaTime);
+    private void CheckForGrounded()
+    {
+        Debug.DrawRay(transform.position, Vector3.down, Color.green);
+        if (Physics.Raycast(transform.position, Vector3.down, out RaycastHit hit, 0.3f))
+        {             
+            if (hit.transform.CompareTag("Ground"))
+            {
+                _isGrounded = true;
+            }
+        }
+        else
+            _isGrounded = false;
+    }
+
+    private void Punch()
+    {
         
+        switch(_punchCombo)
+        {
+            case 0:
+                _punchCombo = 1;
+                _animator.SetTrigger("Punch1");
+                break;
+            case 1:
+                _punchCombo = 0;
+                _animator.SetTrigger("Punch2");
+                break;
+
+        }
+        _characterController.Move(Vector3.zero);
+
     }
 }
